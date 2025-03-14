9cb9c7d101f726cf503567189383246bfcea396e
San7o <santigio2003@gmail.com>
2025-03-14 13:16:21 +0100

    init: nerd-icons, marginalia

---
+++ custom.el
@@ -0,0 +1,24 @@
+  
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("36e7dbc8f9ef915a7dd5b34d7360c0763e583aef28d50ceeed50d445b9702c4a" "76ada746ab3bb01113ae1cd8c42a41f69b187eb8e1e09532c321b7e0fa8f87d7" default))
+ '(package-selected-packages
+   '(orderless swiper nerd-icons-corfu nerd-icons-completion nerd-icons-dired marginalia margianalia brutalist-theme tommyh-theme haskell-mode dockerfile-mode yaml-mode rmsbolt lua-mode zig-mode elpy polymode py-isort tree-sitter go-mode helm-xref yasnippet helm-lsp lsp-mode evil-commentary cmake-mode sudoku elfeed-dashboard elfeed bundler nndiscourse undo-tree diff-hl avy ivy pdf-tools org-drill typit which-key obsidian ## markdown-mode zerodark-theme vterm magit flycheck exwm evil))
+ '(warning-suppress-types '((modus-themes))))
+  ;;(custom-set-faces
+   ;; custom-set-faces was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+  ;;l )
+  
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -6,6 +6,8 @@
 (unless package--initialized (package-initialize))
 
 (load-theme 'modus-vivendi t)
+;; Set default theme to white (I am trying this out)
+(modus-themes-toggle)
 (customize-set-variable 'modus-themes-common-palette-overrides
                         `(
                           ;; Make the mode-line borderless
@@ -69,6 +71,39 @@
 (use-package olivetti
   :ensure t)
 
+(use-package marginalia
+  :ensure t
+  :hook (after-init . marginalia-mode))
+
+(use-package nerd-icons
+  :ensure t)
+
+(use-package nerd-icons-completion
+  :ensure t
+  :after marginalia
+  :config
+  (add-hook 'marginalia-mode-hook #'nerd-icons-completion-marginalia-setup))
+
+(use-package nerd-icons-corfu
+  :ensure t
+  :after corfu
+  :config
+  (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
+
+  (use-package nerd-icons-dired
+    :ensure t
+    :hook (dired-mode . nerd-icons-dired-mode))
+
+(use-package swiper
+  :ensure t)
+
+(use-package orderless
+  :ensure t
+  :custom
+  (setq completion-styles '(orderless basic))
+  (setq completion-category-defaults nil)
+  (setq completion-category-overrides nil))
+
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
@@ -78,6 +113,9 @@
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-message t)
 (setq initial-scratch-message "")
+
+(use-package dired
+  :hook ((dired-mode . dired-hide-details-mode)))
 
 (global-display-line-numbers-mode)
 (setq display-line-numbers 'relative)
@@ -106,6 +144,9 @@
   kept-new-versions 20   ; how many of the newest versions to keep
   kept-old-versions 5    ; and how many of the old
  )
+
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file :no-error-if-file-is-missing)
 
 ;; Monkey-type game
 ;;(require 'typit)
@@ -221,26 +262,3 @@
 
 ;;  (exwm-enable)
 ;; ############### EXWM END ##################
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("36e7dbc8f9ef915a7dd5b34d7360c0763e583aef28d50ceeed50d445b9702c4a" "76ada746ab3bb01113ae1cd8c42a41f69b187eb8e1e09532c321b7e0fa8f87d7" default))
- '(package-selected-packages
-   '(brutalist-theme tommyh-theme haskell-mode dockerfile-mode yaml-mode rmsbolt lua-mode zig-mode elpy polymode py-isort tree-sitter go-mode helm-xref yasnippet helm-lsp lsp-mode evil-commentary cmake-mode sudoku elfeed-dashboard elfeed bundler nndiscourse undo-tree diff-hl avy ivy pdf-tools org-drill typit which-key obsidian ## markdown-mode zerodark-theme vterm magit flycheck exwm evil))
- '(warning-suppress-types '((modus-themes))))
-;;(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-;;l )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
