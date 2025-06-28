1d7e40c4cfe0ae14aaf101e1f0a7991b7c9ca3fa
San7o <santigio2003@gmail.com>
2025-06-28 14:11:05 +0200

    gnus: configured .newsrc file; general cleanup

--- init.el
+++ init.el
@@ -49,6 +49,25 @@
                       :matchers ("begin" "$1" "$" "$$" "\\(" "\\["))))
   (add-hook 'org-mode-hook 'org-fragtog-mode)
 
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory "/home/santo/giovanni-diary/content")
+  :init
+  (setq org-roam-v2-ack t) ;; acknowledge v2
+  :config
+  (org-roam-db-autosync-mode))
+
+;; (use-package org-roam-ui
+;;   :ensure t
+;;   :after org-roam
+;;   :hook (after-init . org-roam-ui-mode)
+;;   :config
+;;   (setq org-roam-ui-sync-theme t
+;;         org-roam-ui-follow t
+;;         org-roam-ui-update-on-save t
+        ;; org-roam-ui-open-on-start t))
+
 (use-package markdown-mode
   :ensure t)
 
