d2989acbbdf07554b7dece58c104a69c019da877
San7o <santigio2003@gmail.com>
2025-07-20 11:20:13 +0200

    emacs: added musync submodule and music file

---
+++ music.el
@@ -0,0 +1,207 @@
+(load-file "musync.el/musync.el")
+
+(musync
+ (list
+  ;; Sleeping at Last/AtlasI
+  (list
+    :name "Overture"
+    :author "Sleeping at Last"
+    :category "Sleeping-at-Last/AtlasI/"
+    :link "https://music.youtube.com/watch?v=2SFCZPGw8WM&si=SS13hdvA3KEMwopo")
+  (list
+    :name "Saturn"
+    :author "Sleeping at Last"
+    :category "Sleeping-at-Last/AtlasI/"
+    :link "https://music.youtube.com/watch?v=dzNvk80XY9s&si=3BfWzf5AKvlgAOuL")
+  (list
+    :name "North"
+    :author "Sleeping at Last"
+    :category "Sleeping-at-Last/AtlasI/"
+    :link "https://music.youtube.com/watch?v=E-iTmEiWcYM&si=Vm0FMoocLOoIuvcM")
+  (list
+    :name "Artic"
+    :author "Sleeping at Last"
+    :category "Sleeping-at-Last/AtlasI/"
+    :link "https://music.youtube.com/watch?v=_bcSDOhcP2Y&si=CAP5aP1s2AiqxPOj")
+  ;; Sleeping at Last/AtlasII
+  (list
+    :name "OvertureII"
+    :author "Sleeping at Last"
+    :category "Sleeping-at-Last/AtlasII"
+    :link "https://music.youtube.com/watch?v=LIx1Hkh3Vqw&si=b_he-WrXJRhL0hdm")
+  (list
+    :name "Nine"
+    :author "Sleeping at Last"
+    :category "Sleeping-at-Last/AtlasII"
+    :link "https://music.youtube.com/watch?v=TijTd60iFDw&si=H87rrhwJHiApm2e1")
+  ;; Sleeping at Last/AtlasIII
+  (list
+    :name "Build"
+    :author "Sleeping at Last"
+    :category "Sleeping-at-Last/AtlasIII/"
+    :link "https://music.youtube.com/watch?v=nYy9aRPgb4o&si=wt290ogVEzI_6b22")
+  (list
+    :name "Destroy"
+    :author "Sleeping at Last"
+    :category "Sleeping-at-Last/AtlasIII/"
+    :link "https://music.youtube.com/watch?v=w8-BFfFr2jI&si=oEzZY0p930jMFvoU")
+  (list
+    :name "Time Lies"
+    :author "Sleeping at Last"
+    :category "Sleeping-at-Last/AtlasIII/"
+    :link "https://music.youtube.com/watch?v=jIaFQNCqmBc&si=FbatA-fdkTATcR9O")
+  (list
+    :name "Doubt"
+    :author "Sleeping at Last"
+    :category "Sleeping-at-Last/AtlasIII/"
+    :link "https://music.youtube.com/watch?v=Egn9GEVEtL4&si=LmDm4RTd9ktaPKsA")
+  (list
+    :name "Doubt"
+    :author "Sleeping at Last"
+    :category "Sleeping-at-Last/AtlasIII/"
+    :link "https://music.youtube.com/watch?v=Egn9GEVEtL4&si=LmDm4RTd9ktaPKsA")
+  ;; Sleeping at Last/Atoms
+  (list
+    :name "Atom 6"
+    :author "Sleeping at Last"
+    :category "Sleeping-at-Last/Atoms"
+    :link "https://music.youtube.com/watch?v=u0MQJlGiDNg&si=LledsKoQaZPOhu2t")
+  ;; Sleeping at Last/Astronomy
+  (list
+    :name "December 25, 2021: Web space Telescope - Launch"
+    :author "Sleeping at Last"
+    :category "Sleeping-at-Last/Astronomy"
+    :link "https://music.youtube.com/watch?v=j5Mf0LYWdaM&si=4wmK_nCHP8oqrNiw")
+  (list
+    :name "June 24, 2022: Parade of Planets"
+    :author "Sleeping at Last"
+    :category "Sleeping-at-Last/Astronomy"
+    :link "https://music.youtube.com/watch?v=ZQ5k0FMpeyA&si=FmkGVERMuxXBr9GI")
+  ;; Ludovico Einaudi
+  (list
+    :name "Nuvole Bianche"
+    :author "Ludovico Einaudi"
+    :category "Ludovico-Einaudi"
+    :link "https://music.youtube.com/watch?v=PhXEL2FRSag&si=q822i4hde3XWhuRq")
+  (list
+    :name "Divenire"
+    :author "Ludovico Einaudi"
+    :category "Ludovico-Einaudi"
+    :link "https://music.youtube.com/watch?v=151eHAC4WJE&si=voZ31pCT-HDUufqh")
+  (list
+    :name "Stella del Mattino"
+    :author "Ludovico Einaudi"
+    :category "Ludovico-Einaudi"
+    :link "https://music.youtube.com/watch?v=nXKkxdQ2WME&si=d3HJZMjrb2Jgy6Ev")
+  (list
+    :name "I Giorni"
+    :author "Ludovico Einaudi"
+    :category "Ludovico-Einaudi"
+    :link "https://music.youtube.com/watch?v=TL1v3KVi6go&si=TPV89A5a6tdmNgYG")
+  (list
+    :name "Due Tramonti"
+    :author "Ludovico Einaudi"
+    :category "Ludovico-Einaudi"
+    :link "https://music.youtube.com/watch?v=Bjwgv9lWrfk&si=077Rxs4VoEnqERol")
+  (list
+    :name "In un'altra vita"
+    :author "Ludovico Einaudi"
+    :category "Ludovico-Einaudi"
+    :link "https://music.youtube.com/watch?v=Bjwgv9lWrfk&si=077Rxs4VoEnqERol")
+  (list
+    :name "Andare"
+    :author "Ludovico Einaudi"
+    :category "Ludovico-Einaudi"
+    :link "https://music.youtube.com/watch?v=SDhbxFm-Nmo&si=Hk_U3knI7RaEKEwl")
+  ;; Folk
+  (list
+    :name "Mazurka Camaieu"
+    :category "Folk-dances"
+    :link "https://music.youtube.com/watch?v=KcNY1qLBA2o&si=upklEDohnmrPo5r-")
+  (list
+    :name "Chapelloise"
+    :category "Folk-dances"
+    :link "https://music.youtube.com/watch?v=DPpNATGbAzg&si=n8uqdenRyvo1fwnh")
+  (list
+    :name "Mazurka de Salou"
+    :category "Folk-dances"
+    :link "https://music.youtube.com/watch?v=jcV55L2S7AQ&si=I-PPp7TJyhFoVT1t")
+  ;; Ambience
+  (list
+    :name "Bloom"
+    :author "Tony Anderson"
+    :category "Ambience"
+    :link "https://music.youtube.com/watch?v=DcxPbLaERR4&si=U1ixlqxMpdNRhuJm")
+  (list
+    :name "Dreamlife"
+    :author "Tony Anderson"
+    :category "Ambience"
+    :link "https://music.youtube.com/watch?v=-7WUMZn8cIY&si=HGafSN16ntGh4DGo")
+  (list
+   :name "Immanuel"
+    :author "Tony Anderson"
+    :category "Ambience"
+    :link "https://music.youtube.com/watch?v=yi2IdZuSgRI&si=Km2-TvijWj-nqieO")
+  (list
+   :name "Eternal Moment"
+   :author "Yutaka Hirasaka"
+    :category "Ambience"
+    :link "https://music.youtube.com/watch?v=0YO7tPyEvwc&si=MSPfToNcXqL60b6J")
+  (list
+    :name "A beautiful day"
+    :author "Yutaka Hirasaka"
+    :category "Ambience"
+    :link "https://music.youtube.com/watch?v=LUZZ8LD_Dck&si=4gMvc5-y8MioTlal")
+  ;; Soundtracks
+  (list
+    :name "The Long Night"
+    :author "Violet Evergarder"
+    :category "Soundtracks"
+    :link "https://music.youtube.com/watch?v=hCAoVP-LNDs&si=XQzOgcatVSHdaAFO")
+  (list
+    :name "Fractored Heart"
+    :author "Violet Evergarder"
+    :category "Soundtracks"
+    :link "https://music.youtube.com/watch?v=-YrsYPcCh9g&si=zKoLX8k8mi8h1Tgk")
+  (list
+    :name "Theme of Ms. Okudera"
+    :author "Your Name"
+    :category "Soundtracks"
+    :link "https://music.youtube.com/watch?v=I4b73WClCWE&si=9BGyBMT5JL37ulX5")
+  (list
+    :name "Spikesong"
+    :author "The Witcher III"
+    :category "Soundtracks"
+    :link "https://music.youtube.com/watch?v=fizQrE_BrTM&si=1UWJOGSZVNN84Vro")
+  (list
+    :name "rev"
+    :author "Koe no Katachi"
+    :category "Soundtracks"
+    :link "https://music.youtube.com/watch?v=ShV3EDNLsls&si=jwRGRloWswAdI-1N")
+  (list
+    :name "Happy Sad Empty Full"
+    :author "C'mon C'mon"
+    :category "Soundtracks"
+    :link "https://music.youtube.com/watch?v=H803Fr8U6jY&si=htHQWKIC36eiaIbj")
+  (list
+    :name "Letter"
+    :author "Silent Hill II"
+    :category "Soundtracks"
+    :link "https://music.youtube.com/watch?v=acQrKPVA32I&si=FGB9pE_lxtHUdh77")
+  ;; Edm
+  (list
+    :name "Save Me"
+    :author "NURKO and Kyle Hume"
+    :category "Edm"
+    :link "https://music.youtube.com/watch?v=gCuvLo4CkUo&si=NpqZUYKDxfSSrl7X")
+  (list
+    :name "Afterdark"
+    :author "MYRNE"
+    :category "Edm"
+    :link "https://music.youtube.com/watch?v=FAJBcrmynUQ&si=S225YshYy9iCztrR")
+  (list
+    :name "Lines of the Borken"
+    :author "DROELOE and San Holo"
+    :category "Edm"
+    :link "https://music.youtube.com/watch?v=ABPCtkLhLBM&si=pdr5U140Kx94M59h")
+  ))
