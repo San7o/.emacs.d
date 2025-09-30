71057cf5bb1c25394b961b0fb4581ede9e705eaf
San7o <santigio2003@gmail.com>
2025-09-30 10:59:51 +0200

    snippets: modified hello snippet in makefile and added instructions

--- snippets/c-mode/.yas-compiled-snippets.el
+++ snippets/c-mode/.yas-compiled-snippets.el
@@ -11,4 +11,4 @@
                        ("hello" "#include <stdio.h>\n\nint main(int argc, char** argv)\n{\n    printf(\"Hello, World!\");\n    $0\n    return 0;\n}" "hello" nil nil nil "/home/santo/.emacs.d/snippets/c-mode/hello" nil nil)))
 
 
-;;; Do not edit! File generated at Tue Sep 16 11:17:19 2025
+;;; Do not edit! File generated at Tue Sep 30 10:56:57 2025
