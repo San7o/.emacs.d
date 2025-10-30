fdfac959b92a3d83a415602c5730831212deb766
San7o <santigio2003@gmail.com>
2025-10-30 21:27:30 +0100

    snippets: added hello snippet in html-mode

---
+++ snippets/html-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; "Compiled" snippets and support files for `html-mode'  -*- lexical-binding:t -*-
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'html-mode
+                     '(("hello" "<!DOCTYPE html>\n<html lang=\"en\">\n  \n  <head>\n    <meta charset=\"UTF-8\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\n    <title>My Website</title>\n    <link rel=\"stylesheet\" href=\"./style.css\">\n    <link rel=\"icon\" href=\"./favicon.ico\" type=\"image/x-icon\">\n  </head>\n  \n  <body>\n    <main>\n        <h1>Welcome to My Website</h1>  \n    </main>\n    <script src=\"index.js\"></script>\n  </body>\n  \n</html>" "hello" nil nil nil "/home/santo/.emacs.d/snippets/html-mode/hello" nil nil)))
+
+
+;;; Do not edit! File generated at Thu Oct 30 21:26:48 2025
--- snippets/markdown-mode/.yas-compiled-snippets.el
+++ snippets/markdown-mode/.yas-compiled-snippets.el
@@ -5,4 +5,4 @@
                      '(("lorem" "Lorem ipsum dolor sit amet." "lorem" nil nil nil "/home/santo/.emacs.d/snippets/markdown-mode/lorem" nil nil)))
 
 
-;;; Do not edit! File generated at Wed Oct 29 14:50:56 2025
+;;; Do not edit! File generated at Thu Oct 30 21:26:48 2025
